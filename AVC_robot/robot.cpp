f0e314e29a65a766f396675d3959edfa24f11560
joelcrampton <66284876+joelcrampton@users.noreply.github.com>
2020-06-11 12:53:26 +1200

    Update robot.cpp

--- AVC_robot/robot.cpp
+++ AVC_robot/robot.cpp
@@ -231,7 +231,7 @@ int main(){
 	bool running = 1;
 	double vLeft = 40.0;
 	double vRight = 30.0;
-	while(running = 1){
+	while(running == 1){
 		takePicture();
 		SavePPMFile("i0.ppm",cameraView);
 		adjustment_degrees = AnalyseImage();
