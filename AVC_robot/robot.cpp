afcf2fdf104ee5665238a3c2cbc7b545f6e888ca
LeifHolm <66284729+LeifHolm@users.noreply.github.com>
2020-06-15 12:33:58 +1200

    Update robot.cpp

--- AVC_robot/robot.cpp
+++ AVC_robot/robot.cpp
@@ -1,44 +1,132 @@
 //Headers
 #include "robot.hpp"
+#include <math.h>
 
-//Namespace
 using namespace std;
 
-//Function Declarations
-
-//Field Declarations
-
+bool doDrive = true;
 
 /**
-* Check if white line exists in given image, return 0 (bad) 1 (good)
+* Check if white line exists in camera view, return 1 (true) or 0 (false)
 */
-int HasWhiteLine(pixel_array) {
-  // for pixels in pixel_array, find white continuous row
-  // if continuous row, 1, else 0
+int HasWhiteLine() {
+	int totalWhite = 0;
+
+	for(int row =0 ; row < cameraView.height ; row++){	
+		for(int column = 0; column < cameraView.width ; column++){
+			int red = (int)get_pixel(cameraView, row, column, 0);
+			int green = (int)get_pixel(cameraView, row, column, 1);
+			int blue = (int)get_pixel(cameraView, row, column, 2);
+			
+			//WHITE DETECTION
+			//All rgb values greater than 250 indicate a white pixel
+			if(red > 250 && green > 250 && blue > 250){
+				totalWhite += 1;
+			}
+		}
+	}
+	//LINE DETECTION
+	//If at least 3x height of frame in white pixels, indicates there is a white line
+	if(totalWhite >= 3*cameraView.height){
+		return 1;
+	}
+	else{
+	    return 0;
+	}
+}
+
+/**
+* Check if red line exists in given image, return 0 (bad) 1 (good)
+*/
+int HasRedLine() {
+	int totalRed = 0;
+
+	for(int row =0 ; row < cameraView.height ; row++){	
+		for(int column = 0; column < cameraView.width ; column++){
+			int red = (int)get_pixel(cameraView, row, column, 0);
+			int green = (int)get_pixel(cameraView, row, column, 1);
+			int blue = (int)get_pixel(cameraView, row, column, 2);
+			
+			//RED DETECTION
+			//Red twice as large as both green and blue indicates a red pixel
+			if(red > 2 * blue and red > 2 * green){
+				totalRed += 1;
+			}
+		}
+	}
+	//LINE DETECTION
+	//If at least 3x height of frame in red pixels, indicates there is a red line
+	if(totalRed >= 3*cameraView.height){
+		return 1;
+	}
+	else{
+	    return 0;
+	}
 }
 
 /**
 * Check if checker black flag exists in given image
 */
-int HasFinish(pixel_array) {
-  // for pixels in pixel_array, find red/white continuous row
-  // if continuous row, 1, else 0
+int HasFinish() {
+	return 0;
 }
 
 /**
-* Returns pixel distance from center of given image to nearest white line, as xy
+* Returns coordinates on white line that robot is aiming for
 */
-int[] GetWhiteTarget(pixel_array) {
-  // for pixels in pixel_array, find center pixel of each row of white
-  // find center of center of white, get degrees to change by
+double GetWhiteTarget() {
+	int xTarget = cameraView.width / 2;
+	int yTarget = cameraView.height - 1;
+	for(int row = cameraView.height - 1 ; row > (cameraView.height / 4) - 1; row -= 1){
+		for(int column = ((3 * cameraView.width) / 8); column < ((5 * cameraView.width) / 8); column++){
+			int r = (int)get_pixel(cameraView, row, column, 0);
+			int g = (int)get_pixel(cameraView, row, column, 1);
+			int b = (int)get_pixel(cameraView, row, column, 2);
+			
+			//WHITE DETECTION
+			//All rgb values greater than 250 indicate a white pixel
+			if(r > 250 && g > 250 && b > 250){
+				if(column < xTarget || column > (cameraView.width - xTarget)){
+					xTarget = column;
+					yTarget = row;
+				}
+			}
+		}
+	}
+	int xRobot = cameraView.width / 2;
+	int yRobot = cameraView.height + 20 - 1;
+	double distX = xTarget - xRobot;
+	double distY = yRobot - yTarget;
+	double theta = atan(distX / distY) * 180 / M_PI;
+	
+	return theta;
 }
 
 /**
-* Returns pixel distance from center of given image to nearest red line
+* Returns coordinates on red wall that robot is aiming for
+* The coordinates are offset so that the robot moves alongside the wall, not on it
 */
-int GetRedTarget(pixel_array) {
-  // for pixels in pixel_array, find first center pixel of each row of red (to find leftmost)
-  // find center of center of red, get degrees to change by (with offset)
+double GetRedTarget() {
+	for(int row =0 ; row < cameraView.height; row++){
+		for(int column = 0; column < cameraView.width ; column++){
+			int r = (int)get_pixel(cameraView, row, column, 0);
+			int g = (int)get_pixel(cameraView, row, column, 1);
+			int b = (int)get_pixel(cameraView, row, column, 2);
+			
+			//RED DETECTION
+			//Red twice as large as both green and blue indicates a red pixel
+			if(r > 2 * b && r > 2 * g){
+				int xRobot = cameraView.width / 2;
+				int yRobot = cameraView.height -1;
+				int xTarget = column + 40; //20px offset from wall
+				int yTarget = row; //20px offset from wall
+				double distX = xTarget - xRobot;
+				double distY = yRobot - yTarget;
+				double theta = atan(distX / distY) * 180 / M_PI;
+				return theta;
+			}
+		}
+	}
 }
 
 /**
@@ -46,31 +134,33 @@ int GetRedTarget(pixel_array) {
 *Else GetRedTarget, and calculate degree adjustment to center robot offset to left red line
 *Return direction to adjust by in degrees
 * */
-double AnalyseImage(pixel_array) {
-  // If HasWhiteLine, theta = GetWhiteTarget
-  // Else theta = GetRedTarget
-  // return theta
+double AnalyseImage() {
+	if(HasWhiteLine() == 1){
+		doDrive = true;
+		return GetWhiteTarget();
+	}
+	else if(HasRedLine() == 1){
+		doDrive = true;
+		return GetRedTarget();
+	}
+	else{
+		doDrive = false;
+	    return 10;
+    }
 }
 
 /**
 *Given input degrees, adjust motor speeds.
 */
 void AdjustRobot(double adjustmentdegrees) {
-  theta = adjustmentdegrees;
-  if (theta <= 180) {
-	  set_Motors(1, 0);
-  } 
-  else {
-	  set_Motors(0,1);
-  }
+  setMotors(adjustmentdegrees, adjustmentdegrees * -1);
 }
 
 /**
 *Do "step", drive at current motor speeds
 */
 void DriveRobot() {
-  set_Motors(1, 1);
-  
+  setMotors(40,40);
 }
 
 /**
@@ -80,32 +170,24 @@ void DriveRobot() {
 *>Conditionals, if colliding, win lose
 *>DriveRobot()
 */
-//int main() {
-  // pixel_array = TakePicture();
-  // adjustment_degrees = AnalyseImage(pixel_array);
-  // AdjustRobot(adjustment_degrees)
-  // Conditionals, if(HasFinish()) etc
-  // End loop
-//}
 
 int main(){
-	if (initClientRobot() !=0){
-		std::cout<<" Error initializing robot"<<std::endl;
+	if(initClientRobot() !=0){
+		cout<<" Error initializing robot"<<endl;
 	}
+	
 	bool running = 1;
-	double vLeft = 40.0;
-	double vRight = 30.0;
-	while(running = 1){
+	while(running == 1){
 		takePicture();
 		SavePPMFile("i0.ppm",cameraView);
-		adjustment_degrees = AnalyseImage(pixel_array);
-		AdjustRobot(adjustment_degrees);
-		DriveRobot();
-		std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
-		usleep(10000);
-		if (HasFinish() = 1) {
+		double adjustmentDegrees = AnalyseImage();
+		AdjustRobot(adjustmentDegrees);
+		if(doDrive) {
+			DriveRobot();
+		}
+		if(HasFinish() == 1) {
 			running = 0;
 		}
 	}
-
+	cout<<"Robot has completed the maze!"<<endl;
 }
